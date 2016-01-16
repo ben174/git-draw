dbe44039fe40ebc078ca6bd374bf73fe8a252a9e
Ben Friedland <ben174@gmail.com>
2016-01-15 19:26:18 -0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -4,9 +4,6 @@ gf = {
         $("svg rect").unbind("click").click(gf.colorCell);
         $("svg rect").on("mouseover").on("mouseover", gf.cellOver);
     },
-
-   
-
     brushMappings: ["#eee", "#d6e685", "#8cc665", "#44a340", "#1e6823"],
     brush: 1,
     brushColor: function() { 
@@ -25,11 +22,18 @@ gf = {
         if (e.buttons==1) {
             gf.colorCell(e);            
         }
-    }
+    },
     render: function() {
+        var ret = "";
         $("svg rect").each(function(i, item) { 
-
+            console.log($(item).attr("data-date"));
+            var dateStr = $(item).attr("data-date");
+            m = dateStr.split("-")[1];
+            y = dateStr.split("-")[0];
+            m = dateStr.split("-")[2];
+            ret += "GIT_AUTHOR_DATE=" + dateStr + "T12:00:00 GIT_COMMITTER_DATE=" + dateStr + "T12:00:00 git commit -a -m \"gf\" > /dev/null echo 1 >> gitfiti\n";
         });
+        console.log(ret);
     }
 }
 
