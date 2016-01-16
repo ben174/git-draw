7887252566720e8c6705f1ae54210270b1ed89e0
Ben Friedland <ben174@gmail.com>
2016-01-15 19:09:10 -0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -2,25 +2,35 @@ gf = {
     init: function() {
         $(".legend>li").unbind("click").click(gf.setBrush);
         $("svg rect").unbind("click").click(gf.colorCell);
+        $("svg rect").on("mouseover").on("mouseover", gf.cellOver);
     },
 
-    brush: null,
+   
 
     brushMappings: ["#eee", "#d6e685", "#8cc665", "#44a340", "#1e6823"],
-
+    brush: 1,
+    brushColor: function() { 
+        return gf.brushMappings[gf.brush];
+    },       
     setBrush: function(e) {
         console.log($(e.target).index());
         console.log($(e.target));
-        var color = gf.brushMappings[$(e.target).index()];
-        gf.brush = color;
-        console.log(gf.brush);
+        gf.brush = $(e.target).index();
     },
 
     colorCell: function(e) {
-        console.log($(e.target).attr("fill"));
-        $(e.target).attr("fill", gf.brush)
+        $(e.target).attr("fill", gf.brushColor());
+    },
+    cellOver: function(e) {
+        if (e.buttons==1) {
+            gf.colorCell(e);            
+        }
     }
+    render: function() {
+        $("svg rect").each(function(i, item) { 
 
+        });
+    }
 }
 
 gf.init();
