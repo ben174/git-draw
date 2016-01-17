cde5fd507f58c78b81733ee60bb97faf26d75697
Ben Friedland <ben174@gmail.com>
2016-01-16 17:47:11 -0800

    Update inject.js

--- inject.js
+++ inject.js
@@ -5,18 +5,18 @@ gf = {
         $("svg rect").on("mouseover").on("mouseover", gf.cellOver);
     },
     brushMappings: ["#eee", "#d6e685", "#8cc665", "#44a340", "#1e6823"],
-    brush: 1,
+    shade: 1,
     brushColor: function() { 
-        return gf.brushMappings[gf.brush];
+        return gf.brushMappings[gf.shade];
     },       
     setBrush: function(e) {
-        console.log($(e.target).index());
-        console.log($(e.target));
-        gf.brush = $(e.target).index();
+        gf.shade = $(e.target).index();
     },
 
     colorCell: function(e) {
+        console.log(e.target);
         $(e.target).attr("fill", gf.brushColor());
+        $(e.target).attr("data-target-shade", gf.shade);
     },
     cellOver: function(e) {
         if (e.buttons==1) {
@@ -27,13 +27,24 @@ gf = {
         var ret = "";
         $("svg rect").each(function(i, item) { 
             console.log($(item).attr("data-date"));
+            var targetShade = gf.brushMappings.indexOf($(item).attr("fill"));
+            if(targetShade==-1) {
+                targetShade = 0;
+            }
+            var existingCommitCount = $(item).attr("data-count");
+            var targetCommitCount = targetShade * 8;
+            var commitsToAdd = targetCommitCount - existingCommitCount;
+            console.log("Target Shade: " + targetShade);            
+            console.log("Current Commit Count: " + existingCommitCount);
+            console.log("Commits to add: " + commitsToAdd);
+
             var dateStr = $(item).attr("data-date");
             m = dateStr.split("-")[1];
             y = dateStr.split("-")[0];
             m = dateStr.split("-")[2];
             ret += "GIT_AUTHOR_DATE=" + dateStr + "T12:00:00 GIT_COMMITTER_DATE=" + dateStr + "T12:00:00 git commit -a -m \"gf\" > /dev/null echo 1 >> gitfiti\n";
         });
-        console.log(ret);
+        //console.log(ret);
     }
 }
 
